45161ee0c156efec4df889b36b40059072d35ff4
Arnaud J <arnaudj@users.noreply.github.com>
2019-04-21 19:51:21 +0200

    Rental: price state pattern: Replace Conditional with Polymorphism for getCharge

--- katas-craftsmanship/src/main/java/org/paumard/devoxxfr2019/tdd/E_MovieRental/model/price/ChildrensPrice.java
+++ katas-craftsmanship/src/main/java/org/paumard/devoxxfr2019/tdd/E_MovieRental/model/price/ChildrensPrice.java
@@ -6,4 +6,13 @@ public class ChildrensPrice extends Price {
     public int getPriceCode() {
         return Movie.CHILDRENS;
     }
+
+    @Override
+    public double getCharge(int daysRented) {
+        double amount = 1.5;
+        if (daysRented > 3)
+            amount += (daysRented - 3) * 1.5;
+        return amount;
+    }
+
 }
--- katas-craftsmanship/src/main/java/org/paumard/devoxxfr2019/tdd/E_MovieRental/model/price/RegularPrice.java
+++ katas-craftsmanship/src/main/java/org/paumard/devoxxfr2019/tdd/E_MovieRental/model/price/RegularPrice.java
@@ -6,4 +6,13 @@ public class RegularPrice extends Price {
     public int getPriceCode() {
         return Movie.REGULAR;
     }
+
+    @Override
+    public double getCharge(int daysRented) {
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+
+        return result;
+    }
 }
