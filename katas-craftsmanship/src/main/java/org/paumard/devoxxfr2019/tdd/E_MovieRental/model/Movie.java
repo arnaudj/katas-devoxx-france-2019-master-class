a6103dee1443cac8e5f4632ff1762575e7e8d2b0
Arnaud J <arnaudj@users.noreply.github.com>
2019-04-21 19:43:55 +0200

    Rental: price state pattern: move getPrice to Price

--- katas-craftsmanship/src/main/java/org/paumard/devoxxfr2019/tdd/E_MovieRental/model/Movie.java
+++ katas-craftsmanship/src/main/java/org/paumard/devoxxfr2019/tdd/E_MovieRental/model/Movie.java
@@ -62,26 +62,8 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        double thisAmount = 0;
-        switch (priceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (daysRented > 2)
-                    thisAmount += (daysRented - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (daysRented > 3)
-                    thisAmount += (daysRented - 3) * 1.5;
-                break;
-
-        }
-        return thisAmount;
+        return _price.getCharge(daysRented);
     }
-
 
     public int getFrequentRenterPoints(int daysRented) {
         if ((priceCode() == Movie.NEW_RELEASE) && daysRented > 1)
